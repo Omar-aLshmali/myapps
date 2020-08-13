725da8fe069fabc7e9f4803b310cc8366463bbfa
omar homs <omaralshmali4@gmail.com>
2020-08-13 20:06:05 +0200

    wird bearbeitet

--- Tik tak toe Spiel/Form1.Designer.cs
+++ Tik tak toe Spiel/Form1.Designer.cs
@@ -28,18 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.sp1 = new System.Windows.Forms.Label();
+            this.sp2 = new System.Windows.Forms.Label();
+            this.unent = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // A00
+            // 
+            this.A00.Location = new System.Drawing.Point(161, 67);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(75, 24);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A01
+            // 
+            this.A01.Location = new System.Drawing.Point(276, 67);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(75, 24);
+            this.A01.TabIndex = 1;
+            this.A01.UseVisualStyleBackColor = true;
+            // 
+            // A02
+            // 
+            this.A02.Location = new System.Drawing.Point(385, 67);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(75, 24);
+            this.A02.TabIndex = 2;
+            this.A02.UseVisualStyleBackColor = true;
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(161, 146);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(75, 24);
+            this.A10.TabIndex = 3;
+            this.A10.UseVisualStyleBackColor = true;
+            // 
+            // A11
+            // 
+            this.A11.Location = new System.Drawing.Point(276, 146);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(75, 24);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            // 
+            // A12
+            // 
+            this.A12.Location = new System.Drawing.Point(385, 146);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(75, 24);
+            this.A12.TabIndex = 5;
+            this.A12.UseVisualStyleBackColor = true;
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(161, 224);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(75, 24);
+            this.A20.TabIndex = 6;
+            this.A20.UseVisualStyleBackColor = true;
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(276, 224);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(75, 24);
+            this.A21.TabIndex = 7;
+            this.A21.UseVisualStyleBackColor = true;
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(385, 224);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(75, 24);
+            this.A22.TabIndex = 8;
+            this.A22.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(276, 300);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(527, 191);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(124, 38);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Neues Spiel";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(526, 276);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(125, 33);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Beenden";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // sp1
+            // 
+            this.sp1.AutoSize = true;
+            this.sp1.Location = new System.Drawing.Point(524, 71);
+            this.sp1.Name = "sp1";
+            this.sp1.Size = new System.Drawing.Size(25, 17);
+            this.sp1.TabIndex = 13;
+            this.sp1.Text = "S1";
+            // 
+            // sp2
+            // 
+            this.sp2.AutoSize = true;
+            this.sp2.Location = new System.Drawing.Point(524, 120);
+            this.sp2.Name = "sp2";
+            this.sp2.Size = new System.Drawing.Size(25, 17);
+            this.sp2.TabIndex = 14;
+            this.sp2.Text = "S2";
+            // 
+            // unent
+            // 
+            this.unent.AutoSize = true;
+            this.unent.Location = new System.Drawing.Point(524, 153);
+            this.unent.Name = "unent";
+            this.unent.Size = new System.Drawing.Size(103, 17);
+            this.unent.TabIndex = 15;
+            this.unent.Text = "Unentschieden";
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(393, 117);
+            this.button1.Location = new System.Drawing.Point(527, 235);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 35);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Neu Anfangen";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -47,14 +191,46 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.unent);
+            this.Controls.Add(this.sp2);
+            this.Controls.Add(this.sp1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A00);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label sp1;
+        private System.Windows.Forms.Label sp2;
+        private System.Windows.Forms.Label unent;
         private System.Windows.Forms.Button button1;
     }
 }
--- Tik tak toe Spiel/Form1.cs
+++ Tik tak toe Spiel/Form1.cs
@@ -12,6 +12,11 @@ namespace Tik_tak_toe_Spiel
 {
     public partial class Form1 : Form
     {
+
+        int spieler;
+        int s1 = 0;
+        int s2 = 0;
+        int unentschieden = 0;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +25,31 @@ namespace Tik_tak_toe_Spiel
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            NeuesSpiel();
+        }
+
+        public void NeuesSpiel()
+        {
+
+            sp1.Text = "Spieler 1 : " + s1.ToString();
+            sp2.Text = "Spieler 2 : " + s2.ToString();
+            unent.Text = unentschieden.ToString();
+
+            
         }
     }
 }
